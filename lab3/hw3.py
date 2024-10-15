65e9f112d9385d43d5fbdc6e693ec31d6fbc4bec
mflkee <makeevgleb86rus@gmail.com>
2024-10-15 22:39:38 +0500

    add hw3

---
+++ lab3/hw3.py
@@ -0,0 +1,11 @@
+items = {
+    'milk15': {'name': 'молоко 1.5%', 'count': 34, 'price': 89.9},
+    'cheese': {'name': 'сыр молочный 1 кг.', 'count': 12, 'price': 990.9},
+    'sausage': {'name': 'колбаса 1 кг.', 'count': 122, 'price': 1990.9}
+}
+
+# Используем dict comprehension для создания нового словаря
+price_less_20 = {key: value['count'] < 20 for key, value in items.items()}
+
+# Вывод результата
+print(price_less_20)
